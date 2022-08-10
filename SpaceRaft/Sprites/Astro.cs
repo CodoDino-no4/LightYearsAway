39ebfad181766307fe9b2da29f5416753048f25f
ALICE CHAMBERS <alicee5cha@gmail.com>
2022-08-10 13:14:26 +0100

    some bits

--- SpaceRaft/Sprites/Astro.cs
+++ SpaceRaft/Sprites/Astro.cs
@@ -24,76 +24,11 @@ namespace SpaceRaft.Sprites
 				{
 
 						state="Idle";
-						Rotation=1f;
 				}
 
 				public override void Update(GameTime gameTime)
 				{
 
-						MoveWithCamera();
 				}
-
-				public Vector2 MoveWithCamera()
-				{
-						_previousKey=_currentKey;
-						_currentKey=Keyboard.GetState();
-
-						var direction = new Vector2((float) Math.Cos(rotation), (float) Math.Sin(rotation));
-
-						if (_currentKey.IsKeyDown(Keys.Up))
-								Position.Y-=direction.Y*LinearVelocity;
-
-						if (_currentKey.IsKeyDown(Keys.Down))
-								Position.Y+=direction.Y*LinearVelocity;
-
-						if (_currentKey.IsKeyDown(Keys.Left))
-								Position.X-=direction.X*LinearVelocity;
-
-						if (_currentKey.IsKeyDown(Keys.Right))
-								Position.X+=direction.X*LinearVelocity;
-
-						return Position;
-				}
-
-				//track elapsed time since last frame, add since the game started
-				// records the time that has elapsed since the last call to the update and other tracks the total time that the game has been running.
-				//ElapsedSeconds = gameTime.ElapsedGameTime.TotalSeconds;
-				//TotalSeconds += ElapsedSeconds;
-
-
-				//if (kstate.IsKeyDown(Keys.Escape))
-				//    Exit();
-
-				//if (Input.IsKeyDown(Keys.Right))
-				//{
-				//    state = PlayerState.Walking;
-
-				//    if (currentVelocity.X > maxVelocity.X)
-				//        currentVelocity = maxVelocity;
-				//    else
-				//        currentVelocity += accelSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-				//}
-
-				//if (state == PlayerState.Idle)
-				//{
-				//    if (currentVelocity.X > 0)
-				//    {
-				//        currentVelocity -= speedDecayRate * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-				//        if (currentVelocity.X < 0)
-				//            currentVelocity = Vector2.Zero;
-
-				//    }
-
-				//    if (currentVelocity.X < Vector2.Zero.X)
-				//    {
-				//        currentVelocity += speedDecayRate * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-				//        if (currentVelocity.X > 0)
-				//            currentVelocity = Vector2.Zero;
-				//    }
-
-				//}
-
 		}
 }
---
+++ SpaceRaft/Sprites/SpriteHandler.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SpaceRaft.Models;
+
+namespace SpaceRaft.Sprites
+{
+		public class SpriteHandler
+		{
+				public Texture2D _texture;
+
+				// Centre of sprite
+				public Vector2 Origin;
+
+				public Vector2 Position;
+				public Vector2 Velocity;
+				protected float rotation;
+				protected string state;
+				public float layer;
+
+				public float RotationVelocity = 10f;
+				public float LinearVelocity = 2f;
+
+				public Input Input;
+				public bool IsRemoved = false;
+
+				protected KeyboardState _currentKey;
+				protected KeyboardState _previousKey;
+
+
+				public SpriteHandler(Texture2D texture)
+				{
+						_texture = texture;
+						Origin = new Vector2(texture.Width/2, texture.Height/2);
+				}
+
+				public Rectangle Rectangle
+				{
+						get
+						{
+								return new Rectangle((int)Position.X, (int)Position.Y, _texture.Width, _texture.Height);
+						}
+				}
+
+				public float Rotation
+				{
+						get
+						{
+								return rotation;
+						}
+						set
+						{
+								rotation=value;
+						}
+				}
+
+				public virtual void Update(GameTime gameTime)
+				{
+
+				}
+
+				// Static sprites
+				public virtual void Draw(SpriteBatch spriteBatch, Vector2 position)
+				{
+						if (_texture!=null)
+						{
+								spriteBatch.Draw(_texture, position, null, Color.White, rotation, Origin, 1, SpriteEffects.None, 0);
+
+						}
+				}
+
+				// Variable position sprites
+				public virtual void Draw(SpriteBatch spriteBatch)
+				{
+						if (_texture!=null)
+						{
+								spriteBatch.Draw(_texture, Position, null, Color.White, rotation, Origin, 1, SpriteEffects.None, 0);
+
+						}
+				}
+		}
+}
